4fac5a68b2c171ecf3545d7bf3a3b24217a669dd
marcoescmont <marcoescmont@gmail.com>
2021-06-02 22:30:54 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,10 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+for (a = 0; a < pronoun.length; a++);
