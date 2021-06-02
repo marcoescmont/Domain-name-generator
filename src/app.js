88abeeb0830a033513babe710fb463b585729c45
marcoescmont <marcoescmont@gmail.com>
2021-06-02 22:53:29 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -7,10 +7,14 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let element = [".com", ".net", ".org", ".us"];
+
+  for (a = 0; a < pronoun.length; a++);
+  {
+  }
+
+  document.getElementById("domain").innerHTML = pronoun[a];
 };
-
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-
-for (a = 0; a < pronoun.length; a++);
