8f8d6885316e3be9d11ecc4d15a09a661ef1d05a
marcoescmont <marcoescmont@gmail.com>
2021-06-04 05:09:58 +0000

    project completed

--- src/app.js
+++ src/app.js
@@ -8,45 +8,24 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-  // function multiplyAll(arr) {
-  //   let product = 1;
-  //   for (let i = 0; i < arr.length; i++) {
-  //     document.getElementById("domain").innerHTML = `${i}${arr[i]}`;
-  //     for (let j = 0; j < arr[i].length; j++) {
-  //       product *= arr[i][j];
-  //     }
-  //   }
-  //   return product;
-  // }
-
-  // multiplyAll([
-  //   ["the", "our"],
-  //   ["great", "big"],
-  //   ["jogger", "racoon"],
-  //   [".com", ".net", ".org", ".us"]
-  // ]);
-
   function getDomain() {
-    let pronoun = ["the", "our"];
+    let pronoun = ["The", "Our", "By"];
     let adj = ["great", "big"];
     let noun = ["jogger", "racoon"];
     let element = [".com", ".net", ".org", ".us"];
 
-    for (let a = 0; a < pronoun.length; a++) {
-      document.getElementById("domain").innerHTML += pronoun[a];
-    }
-    // let a = x - 1;
-    // pronoun.forEach(a => (document.getElementById("pronoun").innerHTML += a));
-    // adj.forEach(b => (document.getElementById("adj").innerHTML += b));
-    // noun.forEach(c => (document.getElementById("noun").innerHTML += c));
-    // element.forEach(d => (document.getElementById("element").innerHTML += d));
-    // pronoun.forEach(a => console.log(a))
-    // adj.forEach(b => console.log(b));
-    // noun.forEach(c => console.log(c));
-    // element.forEach(d => console.log(d));
-  }
+    let domainName = [];
 
-  // document.getElementById("domain").innerHTML = "text";
+    for (let a in pronoun)
+      for (let b in adj)
+        for (let c in noun)
+          for (let d in element)
+            domainName.push(
+              `${"<li>"}${pronoun[a]}${adj[b]}${noun[c]}${element[d]}`
+            );
+
+    document.getElementById("domain").innerHTML = domainName.join("");
+  }
 
   return getDomain();
 };
